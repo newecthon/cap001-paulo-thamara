6ace87dfc8616eff86f7930a696e7b838d020c98
newecthon <ecthon@gmail.com>
2023-04-05 22:53:01 -0300

    styles/Header: config responsive nav item

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,12 @@ import {
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
-const Links = ['Dashboard', 'Projects', 'Team'];
+const Links = [
+  'Home',
+  'Portfolio',
+  'Sobre',
+  'Contato',
+];
 
 const NavLink = ({ children }: { children: ReactNode }) => (
   <Link
@@ -40,16 +45,11 @@ export function Header() {
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={'center'}>
-            <Box>Logo</Box>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'} maxW="1120px" m="0 auto">
+          <Link>Logo here</Link>
+          
+          <Flex alignItems={'center'}>
+            <Menu>
             <HStack
               as={'nav'}
               spacing={4}
@@ -58,30 +58,15 @@ export function Header() {
                 <NavLink key={link}>{link}</NavLink>
               ))}
             </HStack>
-          </HStack>
-          <Flex alignItems={'center'}>
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
             </Menu>
           </Flex>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
         </Flex>
 
         {isOpen ? (
