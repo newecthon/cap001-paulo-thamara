00bcc7886461e6745ee0199a884a8a191738ddc5
newecthon <ecthon@gmail.com>
2023-04-16 13:02:52 -0300

    feat: content portfolio

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,8 +1,9 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
-import { Flex } from "@chakra-ui/react";
+import { Card, CardBody, CardFooter, Flex, Image, Text } from "@chakra-ui/react";
 
 export default function Portfolio() {
+    const myImg = "https://images.unsplash.com/photo-1676107983242-be49d4a542ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
     return (
         <Flex
             direction="column"
@@ -15,10 +16,25 @@ export default function Portfolio() {
                 >
                     <Flex flexWrap="wrap"
                         w="1120px"
-                        h="700px"
+                        h="auto"
                         bg="red"
-                        margin="0 auto"
+                        margin="64px auto"
+                        gap="16px"
                     >
+                        <Flex
+                            w="384px"
+                            h="450px"
+                            backgroundImage={myImg}
+                            backgroundSize={"cover"}
+                            backgroundRepeat={"no-repeat"}
+                            backgroundPosition={"center"}
+                            justify={"center"}
+                            align={"center"}
+                            
+                        >
+                            <Text>Paulo & Thamara</Text>
+                        </Flex>
+                        
                     </Flex>
                 </Flex>
             <Footer />
